5dcd87c352dd1c01c27f6040b4b382c72447b677
Zuzana Oupická <zuzanaoupicka@gmail.com>
2025-01-11 08:40:57 -0500

    Update Fibonacci.js

--- Recursion/Fibonacci.js
+++ Recursion/Fibonacci.js
@@ -11,16 +11,17 @@ function fibs(num) {
   return arr;
 }
 
-// console.log(fibs(2));
+console.log(fibs(2));
 
 function fibsRec(num) {
   if (num === 0) return [];
   if (num === 1) return [0];
+  if (num === 2) return [0, 1];
 
   const arr = fibsRec(num - 1);
+
   arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
   return arr;
 }
 
-// console.log(fibsRec(7));
-
+console.log(fibsRec(7));
