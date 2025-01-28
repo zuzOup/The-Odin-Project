c8f9bd97843a0213581c26b245e1e693654a0f53
Zuzana Oupická <zuzanaoupicka@gmail.com>
2025-01-28 07:23:01 -0500

    fix data

--- CV-Application+Tailwind/src/data.jsx
+++ CV-Application+Tailwind/src/data.jsx
@@ -5,7 +5,7 @@ export const data = {
     title: "Treehouse builder",
     tel: "0124567890",
     email: "willow.skybuilder@real.email",
-    web: "https://zuzoup.github.io/The-Odin-Project/Homepage-responsive/",
+    web: "https://zuzoup.github.io/",
     location: "Branchville, Dreamwood",
     summary:
       "Creative and detail-oriented treehouse builder with a passion for crafting unique, sustainable, and safe elevated structures. Skilled in designing and constructing treehouses that blend seamlessly with natural surroundings, leveraging expertise in carpentry, architectural design, and eco-friendly materials. Adept at collaborating with clients to transform ideas into functional, imaginative spaces. Dedicated to delivering high-quality craftsmanship while ensuring environmental stewardship and structural integrity.",
