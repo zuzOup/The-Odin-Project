e2539132bf0259a127149853e4d19cc9b555ff14
Zuzana Oupická <zuzanaoupicka@gmail.com>
2025-01-31 15:30:14 -0500

    Update Header.tsx

--- Memory-Cards/src/Components/Header.tsx
+++ Memory-Cards/src/Components/Header.tsx
@@ -4,7 +4,10 @@ function Header() {
       <h1>Memory Cards</h1>
       <h2>Get points by clicking on an image but don't click on any more than once!</h2>
       <h3>
-        Made by <a>Zuzana O.</a>
+       By{" "}
+        <a href="https://github.com/zuzOup/" target="_blank">
+          Zuzana O.
+        </a>
       </h3>
     </header>
   );
